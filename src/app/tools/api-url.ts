57637fd15e4c6d5246fcf2e140ae72959930cf0e
aliboura <aliboura@gmail.com>
2021-09-25 11:09:57 +0100

    INTIAL COMMIT

--- src/app/tools/api-url.ts
+++ src/app/tools/api-url.ts
@@ -1,6 +1,7 @@
 // tslint:disable-next-line: variable-name
 export const API_URLs = {
-  AUTH_URL: "http://localhost:8087/auth-api",
+ // AUTH_URL: "http://localhost:8087/auth-api",
+  AUTH_URL: "http://localhost:8091",
   APPS_URL: "http://localhost:8089/d9-api"
   // AUTH_URL: "https://d9test.djezzy.dz:8087/auth-api",
   // APPS_URL: "https://d9test.djezzy.dz:8089/d9-api"
